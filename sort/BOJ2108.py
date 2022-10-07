ced7cc00a7669ee5e3c699a86f6ea7c80e5d4c9b
hannah100-coder <pd206@naver.com>
2022-10-07 22:14:38 +0900

    solved BOJ2108

---
+++ sort/BOJ2108.py
@@ -0,0 +1,52 @@
+#from sys import maxunicode
+import sys
+from collections import Counter
+
+#count = int(input())
+count = int(sys.stdin.readline())
+nums = []
+if count / 2 == 0:
+    print('N은 홀수여야 합니다.')
+
+for i in range(count):
+    #nums.append(int(input()))
+    nums.append(int(sys.stdin.readline()))
+
+nums.sort()
+
+# 산술평균: N개의 수들의 합을 N으로 나눈 값
+print(round(sum(nums)/count))
+
+# 중앙값: N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+print(nums[count // 2])
+
+# 최빈값: N개의 수들 중 가장 많이 나타나는 값
+counter = Counter(nums).most_common()
+if counter[0][1] == counter[1][1]:
+    print(counter[1][0])
+else:
+    print(counter[0][0])
+
+
+# 범위: N개의 수들 중 최댓값과 최솟값의 차이
+print(max(nums) - min(nums))
+
+
+# ===================
+# 최빈값_이전 코드(시간초과)
+# nums_dic = {}
+# for i in nums:
+#     nums_dic[i] = nums.count(i)
+
+# max_exist = max(nums_dic.values())  # 젤 높은 빈도수
+
+# max_num = []
+# for key, value in nums_dic.items():
+#     if value == max_exist:
+#         max_num.append(key)
+
+# if len(max_num) > 1: 
+#     max_num.sort()
+#     print('최빈값: ', max_num[1])
+# else:
+#     print('최빈값: ', max_num[0])
