9d9011273c93c3247a8fc801ef38b55b353d6131
hannah100-coder <pd206@naver.com>
2022-10-08 20:18:52 +0900

    change var name

--- sort/BOJ25305.py
+++ sort/BOJ25305.py
@@ -1,8 +1,8 @@
-student_nums, award_nums = map(int, input().split())
+n, k = map(int, input().split())
 scores = list(map(int, input().split()))
 
-if len(scores) > student_nums:
+if len(scores) > n:
     print('학생 수와 점수 개수가 일치하지 않습니다.')
 
 scores.sort(reverse=True)
-print(scores[award_nums-1])
+print(scores[k-1])
